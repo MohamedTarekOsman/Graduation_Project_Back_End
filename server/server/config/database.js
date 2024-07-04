7e6a66faf67e932871f9a55b4e270c7a04b8658d
MohamedTarek <mt469063@gmail.com>
2024-07-04 17:27:48 +0300

    final

--- server/server/config/database.js
+++ server/server/config/database.js
@@ -6,22 +6,23 @@
 // }
 // module.exports = dbConnection;
 
+
+
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    mongoose.connect('mongodb://localhost:27017/graduation_project', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // Remove useFindAndModify and useCreateIndex options
-    }).then((connection) => {
-        console.log("DB Connected " + connection.connection.host);
-    }).catch((error) => {
-        console.error("DB Connection Error: " + error.message);
-    });
-
-    // Set global options (optional, recommended)
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
-}
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => {
+    console.log("DB Connected " + connection.connection.host);
+  })
+  .catch((err) => {
+    console.error("DB Connection Error: " + err.message);
+    process.exit(1);
+  });
+};
 
 module.exports = dbConnection;
+
